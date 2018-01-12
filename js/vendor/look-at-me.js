f007832019cbaa313cedb0dc55efe1dd846312c7
Amalia Rivera Castillejos <amaliariveracastillejos@gmail.com>
2018-01-12 09:54:01 -0600

    Update look-at-me.js

--- js/vendor/look-at-me.js
+++ js/vendor/look-at-me.js
@@ -1,11 +1,6 @@
 +function () {
     // Elementos
-    var addBoton = document.getElementById("agregar");
-    var showBoton = document.getElementById("mostrar");
-    var searchBoton = document.getElementById("buscar");
-    var topTechBoton = document.getElementById("top-tecnico");
-    var topHSEBoton = document.getElementById("top-hse");
-    var result = document.getElementById("contenedor-estudiantes");
+    
 
     // Evento Click - Agregar
     var addEvent = function (e) {
@@ -23,7 +18,6 @@
     
 
     // Manejadores de eventos
-    addBoton.addEventListener("click", addEvent);
-    showBoton.addEventListener("click", showEvent);
+    
     
 }();
